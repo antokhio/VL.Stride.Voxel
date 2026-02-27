06276abaa75afe8a7d3f6aea721ce729ba5e52fa
antokhio <antokhio@gmail.com>
2026-02-27 20:46:43 +0300

    fix: voxel march

--- VL.Stride.Voxels/src/Lights/LightTypes/VoxelMarcherNodes.cs
+++ VL.Stride.Voxels/src/Lights/LightTypes/VoxelMarcherNodes.cs
@@ -20,23 +20,157 @@ namespace VL.Stride.Voxels.Lights.LightTypes
             _beamDiameter = new(this, (x, v) => x.BeamDiameter = v, 1.0f);
         }
 
-        public void SetSteps(int steps = 9) => _steps.SetValue(steps);
+        private void RebuildIfDirty()
+        {
+            if (!IsDirty)
+                return;
+            Rebuild(instance =>
+            {
+                _steps.ApplyTo(instance);
+                _stepScale.ApplyTo(instance);
+                _beamDiameter.ApplyTo(instance);
+            });
+        }
 
-        public void SetStepScale(float stepScale = 1.0f) => _stepScale.SetValue(stepScale);
+        public void SetSteps(int steps = 9)
+        {
+            _steps.SetValue(steps);
+            RebuildIfDirty();
+        }
 
-        public void SetBeamDiameter(float beamDiameter = 1.0f) =>
+        public void SetStepScale(float stepScale = 1.0f)
+        {
+            _stepScale.SetValue(stepScale);
+            RebuildIfDirty();
+        }
+
+        public void SetBeamDiameter(float beamDiameter = 1.0f)
+        {
             _beamDiameter.SetValue(beamDiameter);
+            RebuildIfDirty();
+        }
     }
 
     /// <summary>
     /// Cone marcher for voxel tracing.
     /// </summary>
     [ProcessNode(Name = "VoxelMarchCone")]
-    public class VoxelMarchConeNode : VoxelNodeImmutable<VoxelMarchCone> { }
+    public class VoxelMarchConeNode : VoxelNodeImmutable<VoxelMarchCone>
+    {
+        private readonly Cachable<bool> _editMode;
+        private readonly Cachable<bool> _fast;
+        private readonly Cachable<int> _steps;
+        private readonly Cachable<float> _stepScale;
+        private readonly Cachable<float> _coneRatio;
+        private readonly Cachable<float> _startOffset;
+
+        public VoxelMarchConeNode()
+        {
+            _editMode = new(this, (x, v) => x.EditMode = v, true);
+            _fast = new(this, (x, v) => x.Fast = v, false);
+            _steps = new(this, (x, v) => x.Steps = v, 9);
+            _stepScale = new(this, (x, v) => x.StepScale = v, 1.0f);
+            _coneRatio = new(this, (x, v) => x.ConeRatio = v, 1.0f);
+            _startOffset = new(this, (x, v) => x.StartOffset = v, 1.0f);
+        }
+
+        private void RebuildIfDirty()
+        {
+            if (!IsDirty)
+                return;
+            Rebuild(instance =>
+            {
+                _editMode.ApplyTo(instance);
+                _fast.ApplyTo(instance);
+                _steps.ApplyTo(instance);
+                _stepScale.ApplyTo(instance);
+                _coneRatio.ApplyTo(instance);
+                _startOffset.ApplyTo(instance);
+            });
+        }
+
+        public void SetEditMode(bool editMode = true)
+        {
+            _editMode.SetValue(editMode);
+            RebuildIfDirty();
+        }
+
+        public void SetFast(bool fast = false)
+        {
+            _fast.SetValue(fast);
+            RebuildIfDirty();
+        }
+
+        public void SetSteps(int steps = 9)
+        {
+            _steps.SetValue(steps);
+            RebuildIfDirty();
+        }
+
+        public void SetStepScale(float stepScale = 1.0f)
+        {
+            _stepScale.SetValue(stepScale);
+            RebuildIfDirty();
+        }
+
+        public void SetConeRatio(float coneRatio = 1.0f)
+        {
+            _coneRatio.SetValue(coneRatio);
+            RebuildIfDirty();
+        }
+
+        public void SetStartOffset(float startOffset = 1.0f)
+        {
+            _startOffset.SetValue(startOffset);
+            RebuildIfDirty();
+        }
+    }
 
     /// <summary>
     /// Cone marcher that traces per mipmap level.
     /// </summary>
     [ProcessNode(Name = "VoxelMarchConePerMipmap")]
-    public class VoxelMarchConePerMipmapNode : VoxelNodeImmutable<VoxelMarchConePerMipmap> { }
+    public class VoxelMarchConePerMipmapNode : VoxelNodeImmutable<VoxelMarchConePerMipmap>
+    {
+        private readonly Cachable<int> _steps;
+        private readonly Cachable<float> _coneRatio;
+        private readonly Cachable<float> _startOffset;
+
+        public VoxelMarchConePerMipmapNode()
+        {
+            _steps = new(this, (x, v) => x.Steps = v, 7);
+            _coneRatio = new(this, (x, v) => x.ConeRatio = v, 1f);
+            _startOffset = new(this, (x, v) => x.StartOffset = v, 0.5f);
+        }
+
+        private void RebuildIfDirty()
+        {
+            if (!IsDirty)
+                return;
+            Rebuild(instance =>
+            {
+                _steps.ApplyTo(instance);
+                _coneRatio.ApplyTo(instance);
+                _startOffset.ApplyTo(instance);
+            });
+        }
+
+        public void SetSteps(int steps = 7)
+        {
+            _steps.SetValue(steps);
+            RebuildIfDirty();
+        }
+
+        public void SetConeRatio(float coneRatio = 1f)
+        {
+            _coneRatio.SetValue(coneRatio);
+            RebuildIfDirty();
+        }
+
+        public void SetStartOffset(float startOffset = 0.5f)
+        {
+            _startOffset.SetValue(startOffset);
+            RebuildIfDirty();
+        }
+    }
 }
