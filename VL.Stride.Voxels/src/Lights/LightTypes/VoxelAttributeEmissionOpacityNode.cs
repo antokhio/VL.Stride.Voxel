0c55442483fe74fbf78d00b3b9e3224a191bff9e
antokhio <antokhio@gmail.com>
2026-02-27 21:04:40 +0300

    rest of props exposed

--- VL.Stride.Voxels/src/Lights/LightTypes/VoxelAttributeEmissionOpacityNode.cs
+++ VL.Stride.Voxels/src/Lights/LightTypes/VoxelAttributeEmissionOpacityNode.cs
@@ -1,5 +1,6 @@
 ﻿using Stride.Rendering.Voxels;
 using VL.Core.Import;
+using static Stride.Rendering.Voxels.VoxelAttributeEmissionOpacity;
 
 namespace VL.Stride.Voxels.Lights.LightTypes
 {
@@ -8,5 +9,50 @@ namespace VL.Stride.Voxels.Lights.LightTypes
     /// </summary>
     [ProcessNode(Name = "VoxelAttributeEmissionOpacity")]
     public class VoxelAttributeEmissionOpacityNode
-        : VoxelNodeImmutable<VoxelAttributeEmissionOpacity> { }
+        : VoxelNodeImmutable<VoxelAttributeEmissionOpacity>
+    {
+        private readonly Cachable<IVoxelLayout> _voxelLayout;
+        private readonly Cachable<IReadOnlyList<VoxelModifierEmissionOpacity>> _modifiers;
+        private readonly Cachable<LightFalloffs> _lightFalloff;
+
+        public VoxelAttributeEmissionOpacityNode()
+        {
+            _voxelLayout = new(this, (x, v) => x.VoxelLayout = v);
+            _modifiers = new(this, (x, v) => x.Modifiers = v?.ToList() ?? []);
+            _lightFalloff = new(this, (x, v) => x.LightFalloff = v, LightFalloffs.Heuristic);
+        }
+
+        private void RebuildIfDirty()
+        {
+            if (!IsDirty)
+                return;
+            Rebuild(instance =>
+            {
+                // Preserve Stride default (new VoxelLayoutIsotropic()) if unset
+                if (_voxelLayout.LastValue is not null)
+                    _voxelLayout.ApplyTo(instance);
+                if (_modifiers.LastValue is not null)
+                    _modifiers.ApplyTo(instance);
+                _lightFalloff.ApplyTo(instance);
+            });
+        }
+
+        public void SetVoxelLayout(IVoxelLayout voxelLayout)
+        {
+            _voxelLayout.SetValue(voxelLayout);
+            RebuildIfDirty();
+        }
+
+        public void SetModifiers(IReadOnlyList<VoxelModifierEmissionOpacity> modifiers)
+        {
+            _modifiers.SetValue(modifiers);
+            RebuildIfDirty();
+        }
+
+        public void SetLightFalloff(LightFalloffs lightFalloff = LightFalloffs.Heuristic)
+        {
+            _lightFalloff.SetValue(lightFalloff);
+            RebuildIfDirty();
+        }
+    }
 }
--- VL.Stride.Voxels/src/Lights/LightTypes/VoxelModifierNodes.cs
+++ VL.Stride.Voxels/src/Lights/LightTypes/VoxelModifierNodes.cs
@@ -8,19 +8,64 @@ namespace VL.Stride.Voxels.Lights.LightTypes
     /// </summary>
     [ProcessNode(Name = "VoxelModifierEmissionOpacityAntiAliasing")]
     public class VoxelModifierEmissionOpacityAntiAliasingNode
-        : VoxelNodeImmutable<VoxelModifierEmissionOpacityAntiAliasing> { }
+        : VoxelNodeImmutable<VoxelModifierEmissionOpacityAntiAliasing>
+    {
+        private readonly Cachable<bool> _enabled;
+
+        public VoxelModifierEmissionOpacityAntiAliasingNode()
+        {
+            _enabled = new(this, (x, v) => x.Enabled = v, true);
+        }
+
+        public void SetEnabled(bool enabled = true)
+        {
+            _enabled.SetValue(enabled);
+            if (IsDirty)
+                Rebuild(instance => _enabled.ApplyTo(instance));
+        }
+    }
 
     /// <summary>
     /// Opacify modifier for voxel emission and opacity.
     /// </summary>
     [ProcessNode(Name = "VoxelModifierEmissionOpacityOpacify")]
     public class VoxelModifierEmissionOpacityOpacifyNode
-        : VoxelNodeImmutable<VoxelModifierEmissionOpacityOpacify> { }
+        : VoxelNodeImmutable<VoxelModifierEmissionOpacityOpacify>
+    {
+        private readonly Cachable<bool> _enabled;
+
+        public VoxelModifierEmissionOpacityOpacifyNode()
+        {
+            _enabled = new(this, (x, v) => x.Enabled = v, true);
+        }
+
+        public void SetEnabled(bool enabled = true)
+        {
+            _enabled.SetValue(enabled);
+            if (IsDirty)
+                Rebuild(instance => _enabled.ApplyTo(instance));
+        }
+    }
 
     /// <summary>
     /// Solidify modifier for voxel emission and opacity.
     /// </summary>
     [ProcessNode(Name = "VoxelModifierEmissionOpacitySolidify")]
     public class VoxelModifierEmissionOpacitySolidifyNode
-        : VoxelNodeImmutable<VoxelModifierEmissionOpacitySolidify> { }
+        : VoxelNodeImmutable<VoxelModifierEmissionOpacitySolidify>
+    {
+        private readonly Cachable<bool> _enabled;
+
+        public VoxelModifierEmissionOpacitySolidifyNode()
+        {
+            _enabled = new(this, (x, v) => x.Enabled = v, true);
+        }
+
+        public void SetEnabled(bool enabled = true)
+        {
+            _enabled.SetValue(enabled);
+            if (IsDirty)
+                Rebuild(instance => _enabled.ApplyTo(instance));
+        }
+    }
 }
