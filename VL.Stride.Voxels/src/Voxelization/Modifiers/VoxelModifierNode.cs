428efa294d1090ec27dbf287a7926fd7a9cef9c1
antokhio <antokhio@gmail.com>
2026-03-03 16:17:57 +0300

    added modifiers

---
+++ VL.Stride.Voxels/src/Voxelization/Modifiers/VoxelModifierNode.cs
@@ -0,0 +1,20 @@
+﻿using Stride.Rendering.Voxels;
+using VL.Core.Import;
+
+namespace VL.Stride.Rendering.Voxels.Voxelization.Modifiers
+{
+    [ProcessNode]
+    public abstract class VoxelModifierNode<TInstance> : ProcessNodeBase<TInstance>
+        where TInstance : VoxelModifier, new()
+    {
+        private readonly Cachable<bool> _enabled;
+
+        public VoxelModifierNode()
+        {
+            _enabled = new(this, (x, v) => x.Enabled = v, true);
+        }
+
+        [Fragment(Order = int.MaxValue)]
+        public void SetEnabled(bool enabled = true) => _enabled.SetValue(enabled);
+    }
+}
