3cf39c85b7718448fbd920e8e5854d3259049523
antokhio <antokhio@gmail.com>
2026-03-03 16:36:02 +0300

    voxelization methods

--- VL.Stride.Voxels/src/Voxelization/FragmentPackers/VoxelFragmentPackaerNodes.cs
+++ VL.Stride.Voxels/src/Voxelization/FragmentPackers/VoxelFragmentPackaerNodes.cs
@@ -1,24 +1,24 @@
 ﻿using Stride.Rendering.Voxels;
 using VL.Core.Import;
 
-namespace VL.Stride.Voxels.Lights.LightTypes
+namespace VL.Stride.Rendering.Voxels.Voxelization.FragmentPackers
 {
     /// <summary>
     /// Packs voxel fragment data as float16.
     /// </summary>
     [ProcessNode(Name = "VoxelFragmentPackFloat16")]
-    public class VoxelFragmentPackFloat16Node : VoxelNodeMutable<VoxelFragmentPackFloat16> { }
+    public class VoxelFragmentPackFloat16Node : ProcessNodeBase<VoxelFragmentPackFloat16> { }
 
     /// <summary>
     /// Packs voxel fragment data as float32.
     /// </summary>
     [ProcessNode(Name = "VoxelFragmentPackFloat32")]
-    public class VoxelFragmentPackFloat32Node : VoxelNodeMutable<VoxelFragmentPackFloat32> { }
+    public class VoxelFragmentPackFloat32Node : ProcessNodeBase<VoxelFragmentPackFloat32> { }
 
     /// <summary>
     /// Packs voxel fragment data as R11G11B10 float.
     /// </summary>
     [ProcessNode(Name = "VoxelFragmentPackFloatR11G11B10")]
     public class VoxelFragmentPackFloatR11G11B10Node
-        : VoxelNodeMutable<VoxelFragmentPackFloatR11G11B10> { }
+        : ProcessNodeBase<VoxelFragmentPackFloatR11G11B10> { }
 }
